616bbedfdf347e386007701b451c4b973781847e
fantasyui-com <captain@fantasyui.com>
2018-05-24 17:21:27 -0400

    Updated Links

--- build.js
+++ build.js
@@ -3,15 +3,15 @@
 const path = require('path');
 const fs = require('fs');
 const request = require('request');
+const kebab = require('lodash.kebabcase');
 
 const program = {
   repos: `https://api.github.com/users/fantasyui-com/repos?page=1&per_page=100`
 };
 
 const head = `# Index Of fantasyui-com Repositories
----
-
 `;
+const complete = [];
 const categories = {};
 
 request({url: program.repos, headers: {'User-Agent': 'Mozilla/3.0 (compatible; MSIE 3.0; Windows NT 5.0)' }}, function (error, response, body) {
@@ -29,14 +29,30 @@ request({url: program.repos, headers: {'User-Agent': 'Mozilla/3.0 (compatible; M
 
 function report(){
   console.log(head);
+
+  console.log(`## Table of Contents`)
+  Object.keys(categories).sort().forEach(function(category){
+    console.log(`- [${category}](#${kebab(category)})`)
+  });
+  console.log(`- [${category}](#${kebab('Index')})`)
+
+  console.log('')
+
   Object.keys(categories).sort().forEach(function(category){
     let list = categories[category]
     console.log(`## ${category}`)
-    list.forEach(function(link){
+    list.sort().forEach(function(link){
       console.log(`- ${link}`)
     })
     console.log('')
-  })
+  });
+
+  console.log(`## Index`);
+  complete.sort().forEach(function(link){
+    console.log(`- ${link}`)
+  });
+  console.log('')
+
 }
 
 function normalizeEntries({name,description}){
@@ -52,8 +68,9 @@ function normalizeEntries({name,description}){
 }
 
 function categorize({tags,md}){
+  complete.push(md);
   tags.forEach(function(tag){
     if(!categories[tag]) categories[tag] = [];
     categories[tag].push(md)
-  })
+  });
 }
